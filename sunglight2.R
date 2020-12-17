2597b4a587087cfef6d355fc2793da6df0a52714
Maria Granell Ruiz <mgranellruiz@gmail.com>
2020-12-17 17:14:15 +0100

    uv data

--- sunglight2.R
+++ sunglight2.R
@@ -12,6 +12,10 @@ library(highcharter)
 library(htmlwidgets) # to save
 
 setwd("/Users/mariagranell/Desktop/Rcourses/TidyverseTuesday")
+
+# --------------------------------------------------
+# DATA GLOBAL RADIATION
+
 d_glov5 <- fromJSON("/Users/mariagranell/Desktop/Rcourses/TidyverseTuesday/fiveyearsglobal.json")
 d_glov5 <- as.data.frame(d_glov5)
 head(d_glov5)
@@ -65,15 +69,14 @@ stat_smooth(geom = "area", method = 'loess', span = 0.3,
 
 cols <- brewer.pal(6, "RdYlBu")
 
-
 # --------------------------------------------------
 # Interactive area plot
-decglobarea <- hchart(d_glov5dec, "area", hcaes(x= day, y = value, group = year)) %>%
+hchart(d_glov5dec, "area", hcaes(x= day, y = value, group = year)) %>%
   hc_colors(cols) %>%
   hc_xAxis(title = list(text="Day")) %>%
   hc_yAxis(title = list(text="Global radiation")) %>%
   hc_title(
-    text = "Global radiation measured in December") %>%
+    text = "Why am I more despressed this year?") %>%
   hc_subtitle( text = "During the frist 10 days comparing 6 years") %>%
   hc_caption( text = "Data: SMHI", align = "right")
   #hc_add_theme(thm)
@@ -109,3 +112,65 @@ saveWidget(decglobarea, file=paste0( getwd(), "/decglobarea.html"))
 # library(ggridges)
 ggplot(d_glov5dec, aes(x = value, y = factor(year))) +
   geom_density_ridges_gradient(scale = 2, gradient_lwd = .9)
+
+# --------------------------------------------------
+# DATA ON UV
+
+d_uv <- fromJSON("/Users/mariagranell/Desktop/Rcourses/TidyverseTuesday/uv2017.json")
+d_uv <- as.data.frame(d_uv)
+head(d_uv)
+str(d_uv)
+
+# cleaning data
+d_uv[] <- lapply(d_uv, gsub, pattern = "T00:00:00Z", replacement = "")
+d_uv$value <- ifelse(d_uv$value < 0, 0, d_uv$value)
+
+# create collums of year, month and day.
+d_uv$split <- strsplit(d_uv$date_time, "-") # you create a list call split in thedat frame
+d_uv[c("year", "month", "day")] <- t(data.frame(d_uv$split)) # to store the list in diff collums
+d_uv <- subset(d_uv, select = -c(split)) # remove the list
+
+# Determine the data type
+d_uv<-within(d_uv,{
+  month<-as.numeric(month)
+  day<-as.numeric(day)
+  year<-as.numeric(year)
+  value<-as.numeric(value)
+  date_time<-as.Date(date_time)
+})
+
+# --------------------------------------------------
+# data just december
+d_uvdec <- subset(d_uv, day < 17)
+d_uvdec <- subset(d_uvdec, month == 12)
+
+d_uvdec$year <- as.factor(d_uvdec$year)
+head(d_uvdec)
+
+cols <- brewer.pal(4, "RdBu")
+# Interactive area plot
+uv <-hchart(d_uvdec, "area", hcaes(x= day, y = value, group = year)) %>%
+  hc_colors(cols) %>%
+  hc_xAxis(title = list(text="Days in December")) %>%
+  hc_yAxis(title = list(text="UV irradiance (mW/m²) in Stockholm")) %>%
+  hc_title(
+    text = "Why am I more despressed this year?") %>%
+  hc_subtitle( text = "The absortion of UV radiation produce vitamin D3") %>%
+  hc_caption( text = "Data: SMHI", align = "right") %>%
+  #hc_add_theme(thm)%>%
+hc_add_annotation(
+  labels = list(
+    list(
+      point = list(
+        xAxis =0 ,
+        yAxis =0 ,
+        x =d_uvdec$day[62],
+        y = d_uvdec$value[62],
+        xAxis=0,
+        yAxis=0
+      ),
+      text = "Everyone taking 'sunny' <br/> pictures for instagram"
+    )
+  )
+)
+saveWidget(uv, file=paste0( getwd(), "/uv.html"))
